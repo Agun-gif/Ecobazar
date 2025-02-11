3ba42d8f60bef316fd08b21ed13c05c97597cbb1
shuvoagun <shuvoagun898@gmail.com>
2025-02-11 15:07:12 +0600

    all

---
+++ src/pages/client/homepage/homepagecomponents/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from './Banner';
+
+function Home() {
+  return (
+    <div>
+      H<Banner/>
+    </div>
+  );
+}
+
+export default Home;
--- src/router/BasicRouter.jsx
+++ src/router/BasicRouter.jsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
+import Banner from "../pages/client/homepage/homepagecomponents/Banner";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element:<MainLayout/>,
     children:[
-      {},
+      {path: "Home", element:<Banner/>},
     ]
   }
 
