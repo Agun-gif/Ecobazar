c62b82c16feca2615143de0d09ed2ed05aabca1d
shuvoagun <shuvoagun898@gmail.com>
2025-02-11 15:46:09 +0600

    all

--- src/router/BasicRouter.jsx
+++ src/router/BasicRouter.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layout/MainLayout.jsx";
-import Home from "../pages/client/homePage/homeComponents/Home.jsx";
+import Home from "../pages/client/homePage/homeComponents/Home.jsx";  // Ensure capital H in Home
 
 
 
@@ -14,15 +14,14 @@ import Home from "../pages/client/homePage/homeComponents/Home.jsx";
 
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <MainLayout />,
-        children: [
-            { path: "Home", element: <Home /> },
-            
-            
-        ],
-    },
+  {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+          { path: "/", element: <Home /> }, // Make the Home page accessible at the root URL '/'
+      ],
+  },
 ]);
 
 export default router;
+
