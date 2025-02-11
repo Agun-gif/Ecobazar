6b77e5dfd2818751c296a44f87d2bcb31b196ffe
shuvoagun <shuvoagun898@gmail.com>
2025-02-11 15:25:44 +0600

    all

--- src/router/BasicRouter.jsx
+++ src/router/BasicRouter.jsx
@@ -1,16 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "../layout/MainLayout";
+import MainLayout from "../layout/MainLayout.jsx";
 import Home from "../pages/client/homePage/homeComponents/Home";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:<MainLayout/>,
-    children:[
-      {path: "Home", element:<Home/>},
-    ]
-  }
 
+
+
+
+
+
+
+
+
+
+
+const routers = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            { path: "Home", element: <Home /> },
+            
+            
+        ],
+    },
 ]);
 
-export default router;
+export default routers;
